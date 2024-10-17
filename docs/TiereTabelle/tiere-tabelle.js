f528922e031b1c4f907a22bab833f642b14399fd
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-10-17 11:47:36 +0200

    HTML-Seite fuer tier-tabelle fertig, aber noch keine JavaScript-Funktion

---
+++ docs/TiereTabelle/tiere-tabelle.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+/**
+ * Diese Funktion wird aufgerufen, wenn das Dokument inkl. aller
+ * Ressourcen (z.B. Bilder oder Stylesheets) geladen wurde.
+ */
+window.addEventListener("load", function () {
+
+    console.log( "Initialisierung abgeschlossen." );
+});
