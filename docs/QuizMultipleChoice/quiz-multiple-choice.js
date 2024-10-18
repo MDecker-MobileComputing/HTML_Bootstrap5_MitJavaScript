10263cd60b006591e0c2c786863f43913a8c1b09
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-10-18 10:59:01 +0200

    Update quiz-multiple-choice.js

--- docs/QuizMultipleChoice/quiz-multiple-choice.js
+++ docs/QuizMultipleChoice/quiz-multiple-choice.js
@@ -45,6 +45,7 @@ window.addEventListener("load", function () {
 
     const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     tooltipTriggerList.map(function (tooltipTriggerEl) {
+
         return new bootstrap.Tooltip(tooltipTriggerEl);
     });
 
