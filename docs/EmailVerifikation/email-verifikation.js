ee2500ca871881b453d720197e0d63c7b4746f7f
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-10-19 21:42:30 +0200

    Email-Verifikation: JavaScript angefangen

--- docs/EmailVerifikation/email-verifikation.js
+++ docs/EmailVerifikation/email-verifikation.js
@@ -1,21 +1,55 @@
 "use strict";
 
 
+let inputLokalerTeil    = null;
+let inputDomain         = null;
+let inputTopLevelDomain = null;
+
+
 /**
  * Diese Funktion wird aufgerufen, wenn das Dokument inkl. aller
  * Ressourcen (z.B. Bilder oder Stylesheets) geladen wurde.
  */
 window.addEventListener("load", function () {
 
-    let pruefenButton = document.getElementById( "pruefenButton" );
-    if ( pruefenButton ) {
+    let buttonPruefen = document.getElementById( "buttonPruefen" );
+    if ( buttonPruefen ) {
 
-        pruefenButton.addEventListener( "click", onLosButton );
+        buttonPruefen.addEventListener( "click", onPruefenButton );
 
     } else {
 
-        console.error( "Button 'Los' nicht gefunden!" );
+        console.error( "Button 'Überprüfen' nicht gefunden!" );
     }
+
+    let buttonLoeschen = document.getElementById( "buttonLoeschen" );
+    if ( buttonLoeschen ) {
+
+        buttonLoeschen.addEventListener( "click", onLoeschenButton );
+
+    } else {
+
+        console.error( "Button 'Zurücksetzen' nicht gefunden!" );
+    }
+
+    inputLokalerTeil = document.getElementById( "inputLokalerTeil" );
+    if ( !inputLokalerTeil ) {
+
+        console.error( "Eingabefeld für lokalen Teil der Email-Adresse nicht gefunden!" );
+    }
+
+    inputDomain = document.getElementById( "inputDomain" );
+    if ( !inputDomain ) {
+
+        console.error( "Eingabefeld für Domain-Teil der Email-Adresse nicht gefunden!" );
+    }
+
+    inputTopLevelDomain = document.getElementById( "inputTopLevelDomain" );
+    if ( !inputTopLevelDomain ) {
+
+        console.error( "Eingabefeld für Top-Level-Domain-Teil der Email-Adresse nicht gefunden!" );
+    }
+
 
     console.log( "Initialisierung abgeschlossen." );
 });
@@ -23,9 +57,23 @@ window.addEventListener("load", function () {
 
 
 /**
- * Event-Handler für den Klick auf den Button "Email-Adresse überprüfen".
+ * Event-Handler für den Klick auf den Button "Überprüfen".
  */
-function onLosButton() {
+function onPruefenButton() {
 
+    console.log( "Email-Adresse wird überprüft..." );
+}
+
+
+/**
+ * Event-Handler für den Klick auf den Button "Zurücksetzen".
+ */
+function onLoeschenButton() {
+
+    console.log( "Eingabefelder werden zurückgesetzt ..." );
+
+    inputLokalerTeil.value    = "";
+    inputDomain.value         = "";
+    inputTopLevelDomain.value = "";    
 }
 
